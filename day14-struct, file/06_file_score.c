4ca02aa17bc75d942b3c258d83f9a71fca8e8165
SemiHedge <soorte214@gmail.com>
2023-04-28 22:18:26 +0900

    Update 06_file_score.c

--- day14-struct, file/06_file_score.c
+++ day14-struct, file/06_file_score.c
@@ -4,31 +4,31 @@
 int main()
 {
     // 파일 포인터 변수 생성
-    FILE *in_file;
-    FILE *out_file;
+    FILE * in_fp;
+    FILE * out_fp;
 
     // 값 연산을 위한 변수
     int num, sum = 0, count = 0;
     float avg = 0.0;
 
     // 읽어 올 파일 포인터 생성하기 - scores.txt
-    in_file = fopen("scores.txt", "r");
-    if (in_file == NULL)
+     in_fp = fopen("scores.txt", "r");
+     if ( in_fp == NULL) 
     {
         printf("Error opening input file\n");
         return 1;
     }
 
     // 내용 쓸 파일 포인터 생성하기 - average.txt
-    out_file = fopen("average.txt", "w");
-    if (out_file == NULL)
+    out_fp = fopen("average.txt", "w");
+    if (out_fp == NULL)
     {
         printf("Error opening output file\n");
         return 1;
     }
 
     // scores.txt에서 점수 정보 가져오고 합산 하기
-    while (fscanf(in_file, "%d", &num) != EOF)
+    while (fscanf( in_fp, "%d", &num) != EOF) 
     {
         sum += num;
         count++;
@@ -39,11 +39,11 @@ int main()
     avg = sum / count;
 
     // average.txt에 기록하기
-    fprintf(out_file, "Average = %.2f", avg);
+    fprintf(out_fp, "Average = %.2f", avg);
 
     // 사용한 파일 포인터 close하여 파일 작업 종료하기
-    fclose(in_file);
-    fclose(out_file);
+    fclose( in_fp);
+    fclose( out_fp);
 
     return 0;
 }
